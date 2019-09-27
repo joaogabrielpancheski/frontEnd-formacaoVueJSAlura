1c68314eda005ff8b58d51eb7b686ef94cc08833
João Gabriel Pancheski <joao.ti@grupointegrado.br>
2019-09-27 17:46:24 -0300

    Aula 06 - Boas práticas

--- webpack.config.js
+++ webpack.config.js
@@ -3,17 +3,42 @@ const babiliPlugin = require('babili-webpack-plugin');
 const extractTextPlugin = require('extract-text-webpack-plugin');
 const optimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const webpack = require('webpack');
+const htmlWebpackPlugin = require('html-webpack-plugin');
 
 let plugins = [];
 
+plugins.push(new htmlWebpackPlugin({
+  hash: true,
+  minify: {
+    html5: true,
+    collapseWhitespace: true,
+    removeComments: true
+  },
+  filename: 'index.html',
+  template: __dirname + '/main.html'
+}))
+
 plugins.push(new extractTextPlugin('styles.css'));
+
 plugins.push(new webpack.ProvidePlugin({
   $: 'jquery/dist/jquery.js',
   jQuery: 'jquery/dist/jquery.js'
 }));
 
+plugins.push(new webpack.optimize.CommonsChunkPlugin({
+  name: 'vendor',
+  filename: 'vendor.bundle.js'
+}));
+
+let SERVICE_URL = JSON.stringify('http://localhost:3000');
+
 if (process.env.NODE_ENV == 'production') {
+  SERVICE_URL = JSON.stringify('http://endereco-da-sua-api');
+
+  plugins.push(new webpack.optimize.ModuleConcatenationPlugin());
+  
   plugins.push(new babiliPlugin());
+
   plugins.push(new optimizeCssAssetsPlugin({
     cssProcessor: require('cssnano'),
     cssProcessorOptions: {
@@ -25,12 +50,16 @@ if (process.env.NODE_ENV == 'production') {
   }))
 }
 
+plugins.push(new webpack.DefinePlugin({ SERVICE_URL }))
+
 module.exports = {
-  entry: './app-src/app.js',
+  entry: {
+    app: './app-src/app.js',
+    vendor: ['jquery', 'bootstrap', 'reflect-metadata']
+  },
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-    publicPath: 'dist'
+    path: path.resolve(__dirname, 'dist')
   },
   module: {
     rules: [
